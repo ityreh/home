a77608cf45292886a387fd334b082916af490aa0
Yannick Rehberger <yr@ityreh.de>
2021-11-23 18:38:05 +0100

    feat(navbar): add navbar component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import Navbar from './components/navbar';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
