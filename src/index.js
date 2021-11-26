786249a1589ff0e617e548dc0cf938b158676fcd
Yannick Rehberger <yr@ityreh.de>
2021-11-26 07:20:39 +0100

    feat(topic): add topic component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Footer from './components/footer';
+import Headline from './components/headline';
 import Navbar from './components/navbar';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
+    <Headline />
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
