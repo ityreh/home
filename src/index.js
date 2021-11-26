5226dfa73db9e9f54824388f0773f16476e337da
Yannick Rehberger <yr@ityreh.de>
2021-11-26 07:03:46 +0100

    chore: delete unused files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
-    <App />
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
